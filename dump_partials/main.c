8920d687d89ee32054c676bede1540fbeda8ba6e
Yifan Lu <me@yifanlu.com>
2017-02-19 11:48:01 -0800

    Added license to main.c

--- dump_partials/main.c
+++ dump_partials/main.c
@@ -1,3 +1,8 @@
+/* Copyright (C) 2017 Yifan Lu
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
 #include <psp2/kernel/processmgr.h>
 #include <psp2/io/fcntl.h>
 #include <psp2/ctrl.h>
